d14217e589dd913d2f786cc2e7c5d0cb354f4d94
xnought <bertuccd@oregonstate.edu>
2024-05-27 22:41:56 -0700

    feat: don't compute extra memory on rng_eca

--- rng_eca.m
+++ rng_eca.m
@@ -1,8 +1,20 @@
-function rng_eca(time_offset)
-    global seed
-    seed_radius = 512;
-    seed = [zeros(1, seed_radius) 1 zeros(1, seed_radius)];
-    % warmup the seed
-    eca_generations = iterate_rule30(seed, seed_radius + time_offset);
-    seed = eca_generations(end, :);
+function rng_eca(offset)
+    % reseeds the rand_eca function for reproducability
+    % time_offset allows you to change the seed time_offset iterations in the future
+
+    % a single black square in the middle surrounded by white
+    % there are seed_radius white cells on the left side then another seed_radius number of white cells on the right side
+    padding = 512;
+    start_state = [zeros(1, padding) 1 zeros(1, padding)];
+
+
+    % when the start state goes past a certain number of iterations (padding number of times), 
+    % we get rid of the pyramid like pattern
+    warmup = padding + offset;
+    for i=1:warmup
+		start_state = next_state_rule30(start_state);
+    end
+
+    global seed % exposed globally so the rand_eca can access the seed and set it when it's done
+    seed = start_state;
 end

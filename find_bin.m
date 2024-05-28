3276f51d3556d0716e9b04f9124224a75a84c5b1
xnought <bertuccd@oregonstate.edu>
2024-05-27 22:49:35 -0700

    removed find_bin

--- find_bin.m
+++
@@ -1,9 +0,0 @@
-function idx = find_bin(r, uniform_bins)
-    idx = length(uniform_bins);
-    for i=1:length(uniform_bins)
-        if r < uniform_bins(i)
-            idx = i;
-            break;
-        end
-    end
-end

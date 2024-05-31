8c75e04d13735ceec134b53e6887d515027f3b33
xnought <bertuccd@oregonstate.edu>
2024-05-31 14:04:04 -0700

    fix

--- uniform_to_pdf.m
+++ uniform_to_pdf.m
@@ -1,14 +1,25 @@
-function [transformed, areas, target_xs, target_ys] = uniform_to_pdf(rand_sample, pdf, t_a, t_b, num_rectangles)
+function [transformed, areas, target_xs, target_ys] = uniform_to_pdf(uniform, pdf, a, b, num_rectangles)
     % compute target mapping areas and what x they correspond to
-    dx = (t_b - t_a) / num_rectangles;
-    target_xs = t_a:dx:(t_b-dx); % rectangle start coordinate x_i
+    dx = (b - a) / num_rectangles;
+    target_xs = a:dx:(b-dx); % rectangle start coordinate x_i
     target_ys = pdf(target_xs); % pdf(x_i) or rectangle height
     areas = dx .* target_ys;  % rectangle widths times heights
     
     % then map the random uniform to those xs from the pdf
     % weighed by the pdf area for that rectangle
-    r = reshape(rand_sample, [], 1); % column vector
-    source_bins = cumsum(areas); % the bins in the uniform distrubtion sized by the pdf areas
-    target_rectangles_indexes = find_thread_over_rows(r < source_bins); % map the rand uniform to a rectangle in the pdf
-    transformed = reshape(target_xs(target_rectangles_indexes), size(rand_sample)); % grab the pdf x_i coordinate from the rectangles
+    r = reshape(uniform, [], 1); % column vector
+    source_bins = cumsum(areas) ./ sum(areas); % the bins in the uniform distrubtion sized by the pdf areas
+    
+    % find what rectangle the uniform maps to in the pdf
+    target_rectangles_indexes = zeros(1, length(r));
+    for i=1:length(r)
+        a_i = find(r(i) < source_bins, 1);
+        if isempty(a_i)
+            target_rectangles_indexes(i) = length(source_bins); % end bin
+        else
+            target_rectangles_indexes(i) = a_i;
+        end
+    end
+
+    transformed = reshape(target_xs(target_rectangles_indexes), size(uniform)); % grab the pdf x_i coordinate from the rectangles
 end

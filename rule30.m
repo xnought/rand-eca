eeadaec79799fda5a6af1b050cb50986e3148523
xnought <bertuccd@oregonstate.edu>
2024-05-31 15:35:38 -0700

    remove comments

--- rule30.m
+++ rule30.m
@@ -1,40 +1,4 @@
 function out_bit = rule30(vec3)
     % Applies rule 30 to a certain corresponding 3 cell states
-    %  ⬛=1, ⬜=0
-
-    % % ⬛⬛⬛
-    % if vec3(1) && vec3(2) && vec3(3)
-    %     out_bit = 0; % ⬜ 
-    
-    % % ⬛⬛⬜
-    % elseif vec3(1) && vec3(2) && ~vec3(3)
-    %     out_bit = 0; % ⬜ 
-
-    % % ⬛⬜⬛
-    % elseif vec3(1) && ~vec3(2) && vec3(3)
-    %     out_bit = 0; % ⬜ 
-
-    % % ⬛⬜⬜
-    % elseif vec3(1) && ~vec3(2) && ~vec3(3)
-    %     out_bit = 1; % ⬛
-
-    % % ⬜⬛⬛
-    % elseif ~vec3(1) && vec3(2) && vec3(3)
-    %     out_bit = 1; % ⬛
-
-    % % ⬜⬛⬜
-    % elseif ~vec3(1) && vec3(2) && ~vec3(3)
-    %     out_bit = 1; % ⬛
-    
-    % % ⬜⬜⬛
-    % elseif ~vec3(1) && ~vec3(2) && vec3(3)
-    %     out_bit = 1; % ⬛
-
-    % % ⬜⬜⬜
-    % else
-    %     out_bit = 0; % ⬜ 
-    % end
-
-    % Which can be condensed down to simple bitwise ops
     out_bit = xor(vec3(1), or(vec3(2), vec3(3)));
 end
